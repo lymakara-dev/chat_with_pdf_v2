3623a98dc0d06c9e49327672cc8c53bb854a93bb
Sokeng99 <shuot1@paragoniu.edu.kh>
2025-01-17 21:56:04 +0700

    update

--- src/components/Sidebar/SidebarArea.vue
+++ src/components/Sidebar/SidebarArea.vue
@@ -58,7 +58,7 @@ const menuGroups = ref([
     <!-- SIDEBAR HEADER -->
     <div class="flex items-center justify-center gap-2 px-6 py-5.5 lg:py-6.5">
       <router-link to="/">
-        <img src="@/assets/images/logo/logo.jpg" alt="Logo" class="h-16 w-auto md:h-20 lg:h-24" />
+        <img src="@/assets/images/logo/amk_logo.png" alt="Logo" class="h-16 w-auto md:h-20 lg:h-24" />
       </router-link>
 
       <button class="block lg:hidden" @click="sidebarStore.isSidebarOpen = false">
